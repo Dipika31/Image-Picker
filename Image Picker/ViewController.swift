b24b84d10f7b478894ee49d5018cc4279711fed5
Dipika Choudhary <dipikachoudhary3104@gmail.com>
2023-03-17 12:22:44 +0530

    Initial Commit

---
+++ Image Picker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Image Picker
+//
+//  Created by Choudhary Dipika on 17/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
