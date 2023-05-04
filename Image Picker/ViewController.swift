afc6bd35e9df894fd723caf654f3e471400fa409
Dipika Choudhary <dipikachoudhary3104@gmail.com>
2023-05-04 11:37:43 +0530

    Done

--- Image Picker/ViewController.swift
+++ Image Picker/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
+    @IBOutlet weak var imagePicker: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.layer.borderWidth = 1
+        imagePicker.layer.borderColor = UIColor.black.cgColor
     }
-
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imagePicker.image = info[.editedImage] as! UIImage
+        dismiss(animated: true)
+    }
+    
+    @IBAction func addImageButton(_ sender: UIButton) {
+        showAlert()
+    }
+    
+    func showAlert()
+    {
+        let alert = UIAlertController.init(title: "Add Image", message: "", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction.init(title: "Camera", style: .default))
+        alert.addAction(UIAlertAction.init(title: "Gallery", style: .default, handler: { alert in
+            
+            let image = UIImagePickerController()
+            image.delegate = self
+            image.sourceType = .photoLibrary
+            image.allowsEditing = true
+            self.present(image, animated: true)
+        }))
+        present(alert, animated: true)
+    }
 }
 
